bccc59ccc89f989f52eb35cb798c8af7abd94ae3
Anukul16 <anukulmaity18@gmail.com>
2024-09-23 23:32:34 +0530

    created repo

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-red-500 mt-10">
+        What's Up Bro
+      </h1>
+    </>
+  );
+}
+
+export default App;
