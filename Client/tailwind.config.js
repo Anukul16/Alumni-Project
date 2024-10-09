07f6626c1d06f4b397822a09992c86245ef74c61
Debasish Das <debasishd849@gmail.com>
2024-10-09 11:06:24 +0530

    first commit

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        header: ['Days One' , 'serif'],
+        body: ['Roboto' , 'sans-serif'],
+        bold:['Roboto-bold' , 'sans-serif']
+
+      },
+     colors:{
+      primary: '#36567C',
+      secondary: '#1d4ed8',
+      text: '#333',
+      background: '#ffffff',
+     }
+    },
   },
   plugins: [],
 }
