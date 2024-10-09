3f1cf5b52689c4c420856b7dedb42a981e294159
Anukul16 <anukulmaity18@gmail.com>
2024-10-09 11:29:21 +0530

    Finished my feature work

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Homepage from './homepage/Homepage';
+// import Home from './Home';
+// import About from './About';
+// import Gallery from './Gallery';
+// import Alumni from './Alumni';
+// import Career from './Career';
+// import EMagazine from './EMagazine';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1 className="text-4xl font-bold text-center text-red-500 mt-10">
-        What's Up Bro
-      </h1>
-    </>
+    <Router>
+      {/* <Navbar /> */}
+      <Homepage />
+      {/* <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/alumni" element={<Alumni />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/e-magazine" element={<EMagazine />} />
+      </Routes> */}
+    </Router>
   );
-}
+};
 
 export default App;
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColor: '#36567c', 
+      },
+    },
+    screens: {
+      'xxs':'350px',
+      'xs': '450px', 
+      'sm': '640px',
+      'md':'768px',
+      'lg':'1024px',
+      'xl':'1280px',
+      '2xl':'1536px'
+      
+    },
   },
   plugins: [],
 }
