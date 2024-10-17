7cd61f836164dbeeeb80ec72fd6c32f6df5f7f6d
Arunava Debnath <arunavadebnath23@gmail.com>
2024-10-17 11:23:36 +0530

    Some changes in Gallary

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import Homepage from "./homepage/Homepage";
+import Homepage from "./homepage/Homepage";
 import "./App.css";
 import "./fonts/fonts.css";
 //import CommonNavbar from "./components/CommonNavbar";
@@ -9,7 +9,10 @@ import Gallery from "./gallary/Gallery";
 const App = () => {
   return (
     <Router>
-      <Gallery />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
     </Router>
   );
 };
