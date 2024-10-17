321be45628364f136377ef80892be4bc885eb011
Arunava Debnath <92048155+AwesomeArunava@users.noreply.github.com>
2024-10-17 11:38:28 +0530

    I make gallery component (#3)
    * I make gallery component, this is very basic. There is someting for work to make it fully dinamic and reuseable
    
    * Some changes in Gallary
    
    * add Link in common Nav commpenent

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,18 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './homepage/Homepage';
-import './App.css';
-import './fonts/fonts.css'
-import CommonNavbar from './components/CommonNavbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./homepage/Homepage";
+import "./App.css";
+import "./fonts/fonts.css";
+//import CommonNavbar from "./components/CommonNavbar";
+import Gallery from "./gallary/Gallery";
 
 const App = () => {
   return (
     <Router>
-      <Homepage />
-      <CommonNavbar/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
     </Router>
   );
 };
