ef62ac5df6ea380879f1680b2bdeb80f24191a6a
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2024-11-09 15:59:10 +0530

    Feature/alumni (#19)
    * Finished my feature work
    
    * conflict solved
    
    * profile completed
    
    * trying again
    
    * this branch done
    
    * login/register done
    
    * updated profile
    
    * edit apis integrated
    
    * skeleton done on profile
    
    * photos done
    
    * fixed issues
    
    * sorry
    
    * filter done

--- Client/src/components/SearchBarWithFilter.jsx
+++ Client/src/components/SearchBarWithFilter.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {Accordion, AccordionItem} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure,Select,SelectItem } from "@nextui-org/react";
 
 const SearchBarWithFilter = ({handleModal}) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filter, setFilter] = useState('all');
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const {isOpen,onOpen,onClose,onOpenChange} = useDisclosure()
   
   const filterOptions = [
     { label: 'All', value: 'all' },
@@ -16,21 +18,26 @@ const SearchBarWithFilter = ({handleModal}) => {
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
-
+  // const openModal = () => {
+  //   onOpen();
+  // }
+  // const closeModal = () => {
+  //   onClose();
+  // }
   const toggleModal = () => {
+    onOpen();
     setIsModalOpen(!isModalOpen);
     handleModal(!isModalOpen);
-
   };
 
   const handleFilterChange = (value) => {
     setFilter(value);
     setIsModalOpen(false); 
   };
-
+  const animals = ["zebra", "tiger", "lion", "elephant", "crocodile", "whale","zebra", "tiger", "lion", "elephant", "crocodile", "whale"]
   return (
     <div className="flex justify-center p-6 z-500">
-      <div className="w-full max-w-2xl p-4 flex space-x-4 items-center">
+      <div className="w-full max-w-2xl p-4 flex space-x-4 items-center"> 
         {/* Search Input */}
         <input
           type="text"
@@ -60,23 +67,87 @@ const SearchBarWithFilter = ({handleModal}) => {
       </div>
 
       {/* Filter Modal */}
-      {isModalOpen && (
+      {
+       (
+          <Modal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          scrollBehavior='inside'
+          onClose={onClose}
+          backdrop="blur"
+          
+          placement='center'
+        >
+        <ModalContent>
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Filter Options
+              </ModalHeader>
+              <ModalBody>
+              <Accordion variant="splitted">
+                <AccordionItem key="1" aria-label="Filter by company" title="Filter by company" className='bg-zinc-300'>
+                  <Select
+                    label="Choose Company"
+                        placeholder="Select a company"
+                    // disabledKeys={["zebra", "tiger", "lion", "elephant", "crocodile", "whale"]}
+                    className="max-w-xs"
+                  >
+                    {animals.map((animal,id) => (
+                      <SelectItem key={id}>
+                        {animal}
+                      </SelectItem>
+                    ))}
+                  </Select>
+                </AccordionItem>
+                <AccordionItem key="2" aria-label="Filter by batch" title="Filter by batch" className='bg-zinc-300'>
+                    <Select
+                        label="Choose Passout Year"
+                        placeholder="Select a year"
+                        // disabledKeys={["zebra", "tiger", "lion", "elephant", "crocodile", "whale"]}
+                        className="max-w-xs"
+                      >
+                        {animals.map((animal,id) => (
+                          <SelectItem key={id}>
+                            {animal}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                </AccordionItem>
+                <AccordionItem key="3" aria-label="Filter by techstack" title="Filter by techstack" className='bg-zinc-300'>
+                    <Select
+                        label="Choose Techstack"
+                        placeholder="Select a techstack"
+                        // disabledKeys={["zebra", "tiger", "lion", "elephant", "crocodile", "whale"]}
+                        className="max-w-xs"
+                      >
+                        {animals.map((animal,id) => (
+                          <SelectItem key={id}>
+                            {animal}
+                          </SelectItem>
+                        ))}
+                      </Select>
+                </AccordionItem>
+              </Accordion>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={()=>{}}>
+                  Apply
+                </Button>
+              </ModalFooter>
+            </>
+          </ModalContent>
+        </Modal>
+        )
+      }
+      {/* {isModalOpen && (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
         <h3 className="text-lg font-medium mb-4">Filter Options</h3>
         <ul>
-          {filterOptions.map((option) => (
-            <li key={option.value} className="mb-2">
-              <button
-                onClick={() => handleFilterChange(option.value)}
-                className={`w-full text-left p-2 rounded-md  ${
-                  filter === option.value ? 'bg-blue-500 text-white' : 'bg-gray-100'
-                } hover:bg-gray-200`}
-              >
-                {option.label}
-              </button>
-            </li>
-          ))}
+        
         </ul>
         <button
           onClick={toggleModal}
@@ -87,7 +158,7 @@ const SearchBarWithFilter = ({handleModal}) => {
       </div>
     </div>
     
-      )}
+      )} */}
     </div>
   );
 };
